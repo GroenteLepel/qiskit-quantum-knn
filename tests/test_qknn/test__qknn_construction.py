b2a237b94453ce0f76db65e9735b2766998bc0eb
GroenteLepel <d.kok@student.science.ru.nl>
2020-10-12 14:47:52 +0200

    Added test for uneven number of train states, and that proper exception is raised when initialising with uneven dimensionality.

--- tests/test_qknn/test__qknn_construction.py
+++ tests/test_qknn/test__qknn_construction.py
@@ -1,21 +1,28 @@
 import logging
 
+import pytest
+
 from qiskit_quantum_knn.qknn._qknn_construction import *
 
 
-def test_create_qknn(caplog):
+def test_create_qknn_2_training_states(caplog):
     caplog.set_level(logging.DEBUG)
-    create_qknn(
+    qknn = create_qknn(
         state_to_classify=[1, 0, 0, 0],
-        classified_states=[
+        classified_states=
+        [
             [1, 0, 0, 0],
             [0, 1, 0, 0]
         ],
-        add_measurement=True
+        add_measurement=False
     )
+    assert qknn.num_qubits == 6, \
+        "qknn with two training states of dim 4 did not create the expected 6" \
+        " qubits qknn. (1 for control, 2 x 2 for states, and 1 for " \
+        "computational basis) "
 
 
-def test_create_oracle(caplog):
+def test_create_oracle_2_training_states(caplog):
     caplog.set_level(logging.DEBUG)
     oracle = create_oracle(
         train_data=
@@ -25,6 +32,33 @@ def test_create_oracle(caplog):
         ]
     )
 
-    assert len(oracle.definition.qubits) == 3, \
-        "train and computational basis registers of size 1 and 2 have not made " \
-        "an oracle of len 3."
+    assert oracle.num_qubits == 3, \
+        "Train and computational basis registers of size 1 and 2 have not " \
+        "made an oracle with 3 qubits."
+
+
+def test_create_oracle_uneven_train_states(caplog):
+    caplog.set_level(logging.DEBUG)
+    oracle = create_oracle(
+        train_data=
+        [
+            [1, 0],
+            [1, 0],
+            [1, 0]
+        ]
+    )
+
+    assert oracle.num_qubits == 3, \
+        "Train and computational basis registers of size 1 and 3 have not " \
+        "made an oracle with 4 qubits."
+
+
+def test_create_oracle_uneven_dim(caplog):
+    with pytest.raises(Exception):
+        caplog.set_level(logging.DEBUG)
+        create_oracle(
+            train_data=
+            [
+                [1, 0, 0]
+            ]
+        )
