63676d1ade4cdd9b17ab81b363084aecea5a5de3
GroenteLepel <d.kok@student.science.ru.nl>
2020-12-21 14:53:10 +0100

    Added ridiculously large test for the qknn

--- tests/test_qknn/test_qkneighborsclassifier.py
+++ tests/test_qknn/test_qkneighborsclassifier.py
@@ -1,4 +1,6 @@
 import numpy as np
+import qiskit as qk
+from qiskit import aqua
 import pytest
 
 from qiskit_quantum_knn.qknn import QKNeighborsClassifier
@@ -37,21 +39,30 @@ def test_setup_control_counts():
     assert True
 
 
+def test_majority_vote_voting():
+    qknn = QKNeighborsClassifier(n_neighbors=3)
+    example_fidelities = np.array([
+        [0.1, 0.9, 0.1, 0.9],
+        [0.9, 0.1, 0.9, 0.1],
+    ])
+    example_labels = np.array([-1, 1, -1, 1])
+    votes = qknn.majority_vote(
+        labels=example_labels,
+        fidelities=example_fidelities
+    )
+    np.testing.assert_array_equal(votes, [1, -1])
+
+
 @pytest.mark.parametrize(
     "n_train_states,n_test_states,n_qubit_states",
     [(8, 1, 8), (8, 8, 8), (5, 8, 8)]
 )
-def test_majority_vote(n_train_states: int,
-                       n_test_states: int,
-                       n_qubit_states: int):
-    if not n_qubit_states:
-        n_qubit_states = n_train_states
-
+def test_majority_vote_result_shape(n_train_states: int,
+                                    n_test_states: int,
+                                    n_qubit_states: int):
     qknn = QKNeighborsClassifier(n_neighbors=3)
-    example_contrasts = np.random.normal(
-        0,
-        size=(n_test_states, n_qubit_states)
-    )
+    example_fidelities = np.random.rand(n_test_states, n_qubit_states)
+
     example_labels = np.random.choice(
         [-1, 1],
         n_train_states
@@ -59,6 +70,38 @@ def test_majority_vote(n_train_states: int,
 
     votes = qknn.majority_vote(
         labels=example_labels,
-        contrasts=example_contrasts
+        fidelities=example_fidelities
     )
     assert votes.shape == (n_test_states,)
+
+
+def test_qknn():
+    backend = qk.BasicAer.get_backend('qasm_simulator')
+    instance = aqua.QuantumInstance(backend, shots=10000)
+
+    # initialising the qknn model
+    qknn = QKNeighborsClassifier(
+        n_neighbors=3,
+        quantum_instance=instance
+    )
+
+    train_data = [
+        [1/np.sqrt(2), 1/np.sqrt(2), 0, 0],
+        [1/np.sqrt(2), 1/np.sqrt(2), 0, 0],
+        [0, 0, 1/np.sqrt(2), 1/np.sqrt(2)],
+        [0, 0, 1/np.sqrt(2), 1/np.sqrt(2)]
+    ]
+    train_labels = [
+        1,
+        1,
+        -1,
+        -1
+    ]
+    test_data = [
+        [1 / np.sqrt(2), 1 / np.sqrt(2), 0, 0],
+        [0, 0, 1 / np.sqrt(2), 1 / np.sqrt(2)]
+    ]
+
+    qknn.fit(train_data, train_labels)
+    qknn_prediction = qknn.predict(test_data)
+    np.testing.assert_array_equal(qknn_prediction, [1, -1])
